900ee495776db6fb218f840e352dd517fcb82f4e
Diego de Almeida Silva <126799322+Z1C0R14@users.noreply.github.com>
2025-04-13 20:30:02 -0300

    RoboSegueLinha.ino

--- RoboSegueLinha.ino
+++ RoboSegueLinha.ino
@@ -1,43 +1,54 @@
+// Aqui é definido os sensores ópticos e em quais entradas do Arduíno estão
 #define pinSensorD 11
 #define pinSensorE 12
 
-#define pinMotorD1 3
-#define pinMotorD2 5
-#define pinMotorE1 6
-#define pinMotorE2 9
+// Aqui é definido a velocidade dos motores
+#define velocidade 80
 
-void setup() {
+// Aqui é definido os sensores dos motor (pra ir pra frente e pra trás) e também onde estão em cada entrada
+#define pinMotorE1 3
+#define pinMotorE2 5
+#define pinMotorD1 6
+#define pinMotorD2 9
+
+void setup(){
   // put your setup code here, to run once:
-  pinMode(pinSensorD, INPUT);
   pinMode(pinSensorE, INPUT);
+  pinMode(pinSensorD, INPUT);
 
-  pinMode(pinMotorD1, OUTPUT);
-  pinMode(pinMotorD2, OUTPUT);
   pinMode(pinMotorE1, OUTPUT);
   pinMode(pinMotorE2, OUTPUT);
+  pinMode(pinMotorD1, OUTPUT);
+  pinMode(pinMotorD2, OUTPUT);
 }
 
-void loop() {
+void loop(){
   // put your main code here, to run repeatedly:
-  estadoD = digitalRead(pinSensorD);
-  estadoE = digitalRead(pinSensorE);
+  bool estadoD = !digitalRead(pinSensorD);
+  bool estadoE = !digitalRead(pinSensorE);
 
-  if(estadoD && estadoE){
-    analogWrite(pinMotorD1, 90);
-    analogWrite(pinMotorE1, 90);
+  if(estadoE && estadoD){
+    analogWrite(pinMotorD1, velocidade);
+    analogWrite(pinMotorD2, velocidade);
+    analogWrite(pinMotorE1, velocidade);
+    analogWrite(pinMotorE2, velocidade);
   }
-  if(!estadoD && !estadoE){
-    digitalWrite(pinMotorD1, LOW);
+  if(!estadoE && !estadoD){
+    analogWrite(pinMotorD1, velocidade);
+    analogWrite(pinMotorD2, velocidade);
+    analogWrite(pinMotorE1, velocidade);
+    analogWrite(pinMotorE2, velocidade);
+  }
+  if(!estadoE && estadoD){
     digitalWrite(pinMotorD2, LOW);
-    digitalWrite(pinMotorE1, LOW);
+    analogWrite(pinMotorD1, velocidade);
     digitalWrite(pinMotorE2, LOW);
+    analogWrite(pinMotorE1, velocidade);
   }
-  if(!estadoD && estadoE){
-    analogWrite(pinMotorD1, 90);
+  if(estadoE && !estadoD){
     digitalWrite(pinMotorE1, LOW);
-  }
-  if(estadoD && !estadoE){
-    analogWrite(pinMotorE1, 90);
+    analogWrite(pinMotorE2, velocidade);
     digitalWrite(pinMotorD1, LOW);
+    analogWrite(pinMotorD2, velocidade);
   }
 }
